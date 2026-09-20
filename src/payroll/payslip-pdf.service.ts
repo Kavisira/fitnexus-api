31de0ca2ba55e9c9c739e15bc208a76aee79a667
Kavisira <kavisira@Mac.lan>
2026-09-20 15:32:02 +0530

    build issue fixed

--- src/payroll/payslip-pdf.service.ts
+++ src/payroll/payslip-pdf.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as PDFDocument from 'pdfkit';
+import PDFDocument = require('pdfkit');
 import { Payslip, Employee, Branch, Organization } from '@prisma/client';
 
 const MONTH_NAMES = [
